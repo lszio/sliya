b2d5c1d198264b8ec0eaad3970ac3994442a594c
Liszt21 <1832666492@qq.com>
2021-09-07 09:44:27 +0800

    add security

--- sliya/__init__.py
+++ sliya/__init__.py
@@ -1,15 +1,24 @@
 __version__ = '0.1.0'
 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 import uvicorn
 
 app = FastAPI()
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get("/ping")
 async def ping():
     return {"message": "Pong!!!"}
+
+
+@app.get("/token")
+async def get_token(token: str = Depends(oauth_scheme)):
+    return {"token": token}
+
 
 def run_dev_server():
     uvicorn.run("sliya:app", host="localhost", port=9715, log_level="debug")
